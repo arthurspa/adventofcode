59cdf93015f8e023e153219e4b2b4d01fc52888a
Arthur Silva <arthursilva.ee@gmail.com>
2018-12-11 22:23:15 +0100

    Load same input file for challagens of same day

--- runner.py
+++ runner.py
@@ -8,7 +8,14 @@ filename = filename.split('.')[0]
 module = importlib.import_module(f'{year_folder}.{filename}')
 
 if getattr(module, 'solve', False):
-    with open(f'{year_folder}/input/{filename}') as read:
-        module.solve(read)
+    file_path = f'{year_folder}/input/{filename}'
+    if not os.path.isfile(file_path):
+        file_path = file_path.split('_')[0]  # remove _1 or _2
+
+    if not os.path.isfile(file_path):
+        print('Missing input file?')
+    else:
+        with open(file_path) as read:
+            module.solve(read)
 else:
     print('Missing solve function?')
