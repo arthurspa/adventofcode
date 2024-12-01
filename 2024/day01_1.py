62c1eedfd254d9c97157f4fe902de6e6341fede2
Arthur Silva <arthur.silva@ericsson.com>
2024-12-01 06:22:26 +0100

    2024 day 01 p1 p2

--- 2024/day01_1.py
+++ 2024/day01_1.py
@@ -2,5 +2,17 @@ from io import TextIOWrapper
 
 
 def solve(input: TextIOWrapper):
-    pass
+    left, right = [], []
+    for line in input:
+        l, r = [int(x) for x in line.split(" ") if x != ""]
+        left.append(l)
+        right.append(r)
 
+    left.sort()
+    right.sort()
+
+    diff = []
+    for i in range(len(left)):
+        diff.append(abs(left[i] - right[i]))
+
+    print(diff)
--- 2024/day01_2.py
+++ 2024/day01_2.py
@@ -2,5 +2,22 @@ from io import TextIOWrapper
 
 
 def solve(input: TextIOWrapper):
-    pass
+    left = []
+    freq = dict()
+    for line in input:
+        l, r = [int(x) for x in line.split(" ") if x != ""]
+        left.append(l)
+        if r in freq:
+            freq[r] += 1
+        else:
+            freq[r] = 1
 
+    diff = []
+    for i in range(len(left)):
+        mult = 0
+        if left[i] in freq:
+            mult = freq[left[i]]
+
+        diff.append(left[i] * mult)
+
+    print(sum(diff))
